852663c57fb88dfec95c0adf5e722eea2266c238
hyebb <hyebb@ncsoft.com>
2022-03-11 11:20:11 +0900

    1~24

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,105 @@
 package com.learnreactiveprogramming.service;
 
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+
 public class FluxAndMonoGeneratorService {
+
+    /**
+     * Flux
+     */
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("so", "hye", "been")); //db or a remote service call
+    }
+
+    public Flux<String> namesFlux_map() {
+        return Flux.fromIterable(List.of("so", "hye", "been"))
+//                .map(s -> s.toUpperCase());
+                .map(String::toUpperCase) // immutable : always return new Flux
+                .log();
+    }
+
+    public Flux<String> namesFlux_filter(int len) {
+        return Flux.fromIterable(List.of("so", "hye", "been"))
+                .filter(s -> s.length() > len)
+                .log();
+    }
+
+    public Flux<String> namesFlux_filter_map(int len) {
+        return Flux.fromIterable(List.of("so", "hye", "been"))
+                .filter(s -> s.length() > len)
+                .map(s -> s.length() + "-" + s)
+                .log();
+    }
+
+    public Flux<String> namesFlux_flatmap(int len) {
+        return Flux.fromIterable(List.of("so", "hye", "been"))
+                .filter(s -> s.length() > len)
+                .flatMap(s -> Flux.fromArray(s.split("")))
+                .log();
+    }
+
+    public Flux<String> namesFlux_flatmap_map(int len) {
+        return Flux.fromIterable(List.of("so", "hye", "been"))
+                .filter(s -> s.length() > len)
+                .map(s -> s.split(""))
+                .flatMap(s -> Flux.fromArray(s))
+                .log();
+    }
+
+    public Flux<String> namesFlux_flatmap_async(int len) {
+        return Flux.fromIterable(List.of("so", "hye", "been"))
+                .filter(s -> s.length() > len)
+                .flatMap(s -> splitString_withDelay(s))
+                .log();
+    }
+
+    public Flux<String> namesFlux_concatmap_async(int len) {
+        return Flux.fromIterable(List.of("so", "hye", "been"))
+                .filter(s -> s.length() > len)
+                .concatMap(s -> splitString_withDelay(s))
+                .log();
+    }
+
+    private Flux<String> splitString_withDelay(String str) {
+        String[] arr = str.split("");
+//        int delay = new Random().nextInt(3000);
+        int delay = 1000;
+        return Flux.fromArray(arr)
+                .delayElements(Duration.ofMillis(delay)); //have a delay before emitting each and every element from this flux
+    }
+
+    public Flux<String> logFlux() {
+        return Flux.fromIterable(List.of("so", "hye", "been")).log();
+    }
+
+    /***
+     * Mono
+     */
+    public Mono<String> nameMono() {
+        return Mono.just("hyebeen");
+    }
+
+    public Mono<String> logMono() {
+        return Mono.just("hyebeen").log();
+    }
+
+    /**
+     * Main
+     */
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+//        the only way to access these values is by subscribing to that flux/Mono
+        fluxAndMonoGeneratorService.namesFlux().subscribe(name -> System.out.println("Name is " + name)); // way1. subscribe(consumer)
+        fluxAndMonoGeneratorService.nameMono().subscribe(name -> System.out.println("Mono Name is " + name)); // way1. subscribe(consumer)
+        fluxAndMonoGeneratorService.logFlux().subscribe(name -> System.out.println("value : " + name));
+        fluxAndMonoGeneratorService.logMono().subscribe(name -> System.out.println("value : " + name));
+
+
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,139 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFluxTest1() {
+        //given
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux();
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("so", "hye", "been")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxTest2() {
+        //given
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux();
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("hye", "so", "been") // "so", "hye", "been"
+                .verifyComplete(); //err
+    }
+
+    @Test
+    void namesFluxTest3() {
+        //given
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux();
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxTest4() {
+        //given
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux();
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("so")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        //given
+        //when
+        var nameFluxVar = fluxAndMonoGeneratorService.namesFlux_map();
+        //then
+        StepVerifier.create(nameFluxVar)
+                .expectNext("SO", "HYE", "BEEN")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_filter() {
+        //given
+        int len = 3;
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux_filter(len);
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("been")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_filter_map() {
+        //given
+        int len = 3;
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux_filter_map(len);
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("4-been")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatmap() {
+        //given
+        int len = 2;
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux_flatmap(len);
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("h","y","e","b","e","e","n")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatmap_map() {
+        //given
+        int len = 2;
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux_flatmap_map(len);
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("h","y","e","b","e","e","n")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatmap_async() {
+        //given
+        int len = 2;
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux_flatmap_async(len);
+        //then
+        StepVerifier.create(namesFluxVar)
+//                .expectNext("h","y","e","b","e","e","n")
+                .expectNextCount("hyebeen".length())
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_concatmap_async() {
+        //given
+        int len = 2;
+        //when
+        var namesFluxVar = fluxAndMonoGeneratorService.namesFlux_concatmap_async(len);
+        //then
+        StepVerifier.create(namesFluxVar)
+                .expectNext("h","y","e","b","e","e","n")
+//                .expectNextCount("hyebeen".length())
+                .verifyComplete();
+    }
 }
